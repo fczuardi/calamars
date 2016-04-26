d3f3c84319b83499e58bba4b4c37e1499bcb509f
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-26 11:11:54 -0300

    remove the one liners that generated the callbackPairs to improve readability

--- test/answers.js
+++ test/answers.js
@@ -4,49 +4,60 @@ import {
     createRegexRouter
 } from 'lib/answers';
 
-const stringPairs = [
-    ['yes', 'no'],
-    ['stop', 'go go go'],
-    ['goodbye', 'hello'],
-    ['high', 'low'],
-    ['why', 'I don’t know']
-];
 test('string -> string: Hello/Goodbye exact match router', t => {
+    const stringPairs = [
+        ['yes', 'no'],
+        ['stop', 'go go go'],
+        ['goodbye', 'hello'],
+        ['high', 'low'],
+        ['why', 'I don’t know']
+    ];
     const router = createExactMatchRouter(stringPairs);
     t.is(router('high'), 'low');
     t.is(router('cha cha cha'), null);
 });
 
-const regexPairs = [
-    [/yes/i, 'no'],
-    [/stop/i, 'go go go'],
-    [/goodbye/i, 'hello'],
-    [/high/i, 'low'],
-    [/why/i, 'I don’t know']
-];
 test('regex -> string: Hello/Goodbye regex router', t => {
+    const regexPairs = [
+        [/yes/i, 'no'],
+        [/stop/i, 'go go go'],
+        [/goodbye/i, 'hello'],
+        [/high/i, 'low'],
+        [/why/i, 'I don’t know']
+    ];
     const router = createRegexRouter(regexPairs);
     t.is(router('I say HIGH, you say?'), 'low');
     t.is(router('cha cha cha'), null);
 });
 
-const callbacks = {
-    yes() { return 'no'; },
-    halt() { return 'go go go'; },
-    goodbye() { return 'hello'; },
-    high() { return 'low'; },
-    why() { return 'I don’t know'; }
-};
-const cbKeys = Object.keys(callbacks);
-const stringCbPairs = stringPairs.map((pair, i) => [pair[0], callbacks[cbKeys[i]]]);
-test('string -> callback: Hello/Goodbye exact match router', t => {
+test('string -> callback -> string : Hello/Goodbye exact match router', t => {
+    const callbacks = {
+        yes() { return 'no'; },
+        halt() { return 'go go go'; },
+        goodbye() { return 'hello'; },
+        high() { return 'low'; },
+        why() { return 'I don’t know'; }
+    };
+    const stringCbPairs = [
+        ['yes', callbacks.yes],
+        ['stop', callbacks.halt],
+        ['goodbye', callbacks.goodbye],
+        ['high', callbacks.high],
+        ['why', callbacks.why]
+    ];
     const router = createExactMatchRouter(stringCbPairs);
     t.is(router('high')(), 'low');
     t.is(router('cha cha cha'), null);
 });
 
-const regexCbPairs = regexPairs.map((pair, i) => [pair[0], callbacks[cbKeys[i]]]);
-test('regex -> callback: Hello/Goodbye regex router', t => {
+test('regex -> callback -> string: Hello/Goodbye regex router', t => {
+    const regexCbPairs = [
+        [/yes/i, () => 'yes'],
+        [/stop/i, () => 'go go go'],
+        [/goodbye/i, () => 'hello')],
+        [/high/i, () => 'low'],
+        [/why/i, () => 'I don’t know']
+    ];
     const router = createRegexRouter(regexCbPairs);
     t.is(router('high')(), 'low');
     t.is(router('cha cha cha'), null);
