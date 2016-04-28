239fa626f29fe13aa8418b76c7154fae89a56694
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-28 12:01:14 -0300

    documentation for the index file

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,21 @@
+// Calamars
+// ========
+//
+// An alpha quality, under heavy development, proto-frramework for building
+// conversational applications.
+//
+// This library is a collection of different helper functions and classes,
+// currently there are 2 main components:
+//
+// - [router.js](./router.html)
+//   - a helper function to create input > output mappings
+// - [luis.js](./luis.html)
+//   - a driver for the LUIS webservice
+//
+// ---
+//
+// Source
+// ------
 export { version } from '../../package.json';
-export {
-    createExactMatchRouter,
-    createRegexRouter,
-    createRegexFunctionRouter,
-    createRouter
-} from './router';
-export {
-    previewBaseURL,
-    previewApiPath,
-    LuisDriver
-} from './luis';
+export * from './router';
+export * from './luis';
