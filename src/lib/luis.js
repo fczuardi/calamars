81ece83b648a3edae337c8c7ecfba24ecb5908bc
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-29 15:16:35 -0300

    basic documentation of the current LuisDriver

--- src/lib/luis.js
+++ src/lib/luis.js
@@ -1,3 +1,8 @@
+// LuisDriver
+// ==========
+//
+// A wrapper class for the LUIS Webservice (https://www.luis.ai/ ).
+//
 import request from 'request-promise';
 
 const previewBaseURL = 'https://api.projectoxford.ai';
@@ -5,6 +10,11 @@ const previewApiPath = '/luis/v1/application/preview';
 const previewURL = previewBaseURL + previewApiPath;
 
 class LuisDriver {
+    // ## Constructor
+    // ### Parameters
+    // - **options** - _Object_ - Configuration object.
+    //   - **id** - _string_ - A LUIS app ID.
+    //   - **subscriptionKey** - _string_ - A LUIS subscription key.
     constructor(options) {
         const {
             id,
@@ -20,10 +30,37 @@ class LuisDriver {
         this.brainURL = `${previewURL}?id=${id}&subscription-key=${subscriptionKey}`;
     }
 
+    // ### Methods
+    //
+    // #### query
+    // ##### Parameters
+    // - **text** - _string_ - a text query to send to the LUIS webservice.
+    //
+    // ##### return
+    // Returns a Promise that returns the parsed object from the webservice.
     query(text = '') {
         const url = `${this.brainURL}&q=${text}`;
         return request(url).then(body => JSON.parse(body));
     }
 }
 
+// ## Examples
+// ```javascript
+// const options = {
+//     id: 'YOUR_LUIS_ID',
+//     subscriptionKey: 'YOUR_SUBSCRIPTION_KEY'
+// };
+// const luis = new LuisDriver(options);
+// luis.query('Hi')
+//     .then(result => {
+//         const {
+//             intent,
+//             score
+//         } = result.topScoringIntent;
+//         console.log(result.query); // 'Hi'
+//         console.log(intent); // 'None'
+//         console.log(score); // 0.9042053
+//     });
+// ```
+// See [test/luis.js](https://github.com/fczuardi/calamars/blob/master/test/luis.js) for more examples.
 export { previewBaseURL, previewApiPath, LuisDriver };
