fbf84abd722f86c3f9b6254ea269d7bf3704458a
Frederico Marques <frederico.marques.22@gmail.com>
2016-08-12 16:06:00 -0300

    S3 support implemented

---
+++ src/lib/s3Storage.js
@@ -0,0 +1,107 @@
+import config from '../../s3Config';
+import AWS from 'aws-sdk';
+import {
+    prop, keys
+} from 'ramda';
+
+AWS.config.update(config.AWS);
+const path = config.path;
+
+const getDb = param => new AWS.S3(param.S3);
+
+// Just for compability
+const getUsers = db => true;  // eslint-disable-line
+const setUsers = (db, data) => true; // eslint-disable-line
+
+const getUser = (db, userId) =>
+    db.getObject({
+        Key: `${path}/${userId}.json`
+    }).promise()
+    .then(body => body.Body.toString())
+    .catch(err => {
+        console.log(err);
+        return {};
+    });
+
+const setUser = (db, userId, nextUser) => {
+    if (!nextUser) { return null; }
+    return db.putObject({
+        Key: `${path}/${userId}.json`,
+        Body: JSON.stringify(nextUser)
+    }).promise()
+    .then(() => userId)
+    .catch(err => {
+        console.log(err);
+        return nextUser;
+    });
+};
+
+const removeUser = (db, userId) => {
+    if (!userId) { return null; }
+    return db.deleteObject({
+        Key: `${path}/${userId}.json`
+    }).promisse()
+    .then(() => userId)
+    .catch(err => {
+        console.log(err);
+        return undefined;
+    });
+};
+
+const getUserProp = (db, userId, key) => {
+    if (!userId) { return null; }
+    return getUser(db, userId)
+    .then(u => {
+        if (!u || !keys(u).length) { return undefined; }
+        const user = JSON.parse(u);
+        if (!key) {
+            return '';
+        }
+        return prop(key, user);
+    }).catch(err => {
+        console.log(err);
+        return undefined;
+    });
+};
+
+const setUserProp = (db, userId, key, value) => {
+    if (!userId) { return null; }
+    return getUser(db, userId)
+    .then(u => {
+        if (!u || !keys(u).length) { return undefined; }
+        const user = JSON.parse(u);
+        user[key] = value;
+        return user;
+    })
+    .catch(err => {
+        console.log(err);
+        return undefined;
+    });
+};
+
+const removeUserProp = (db, userId, key) => {
+    if (!userId || !key) { return null; }
+    return getUser(db, userId)
+    .then(u => {
+        if (!u || !keys(u).length) { return undefined; }
+        const { [key]: oldItem, ...other} = JSON.parse(u); // eslint-disable-line
+        return setUser(db, userId, { ...other });
+    })
+    .catch(err => {
+        console.log(err);
+        return undefined;
+    });
+};
+
+
+export {
+    getDb,
+    getUsers,
+    setUsers,
+    getUser,
+    setUser,
+    removeUser,
+    getUserProp,
+    setUserProp,
+    removeUserProp
+};
