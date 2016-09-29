8f0556ce9a2f756ee925b106197535cd74652182
Fabricio C Zuardi <fabricio@fabricio.org>
2016-09-29 03:10:35 -0300

    add isEcho property on calamar formatted messages

--- src/lib/calamar.js
+++ src/lib/calamar.js
@@ -21,11 +21,12 @@ const isTelegramUpdate = update => (update && update.update_id);
 const calamarMessageFormat = update => {
     if (isFacebookMessengerMessagingItem(update)) {
         const { message, timestamp, sender, recipient } = update;
-        const { mid = null, text = null } = message;
+        const { mid = null, text = null, is_echo = false } = message;
         return {
             text,
             timestamp,
             messageId: mid,
+            isEcho: is_echo,
             senderId: sender.id,
             recipientId: recipient.id,
             chatId: sender.id,
